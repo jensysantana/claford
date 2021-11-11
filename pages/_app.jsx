4896daca464b38da823eff315d900bc6398272e8
jensysantana <jensysantana@gmail.com>
2021-11-10 23:30:46 -0500

    auth test9

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -35,8 +35,9 @@ function App({ Component, pageProps }) {
 
     useEffect(() => {
         async function getCsrf() {
-            const resp = await Api.AUTH.getCsrfAuth();
-            setstate(() => resp.data.token);
+            // const resp = await Api.AUTH.getCsrfAuth();
+            // setstate(() => resp.data.token);
+            setstate(() => '');
         }
         getCsrf();
 
